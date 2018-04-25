466cc5990544c654524a91cd3a9492a6a66cff5b
PatiKoszi <patrykkosznik@gmail.com>
2018-04-25 16:36:00 +0200

    Add component TaskPostpone

---
+++ src/components/TaskPostpone/TaskPostpone.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './TaskPostpone.css';
+
+class TaskPostpone extends Component {
+  render() {
+    return (
+
+      <button>Postpone</button>
+
+    );
+  }
+}
+
+export default TaskPostpone;
