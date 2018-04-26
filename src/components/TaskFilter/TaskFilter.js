3791ca46e4ced30a402564cc02bfa2a78f2c313f
BaldiniPL <baldakamil@gmail.com>
2018-04-26 16:27:34 +0200

    filter buttons added

--- src/components/TaskFilter/TaskFilter.js
+++ src/components/TaskFilter/TaskFilter.js
@@ -8,10 +8,10 @@ class TaskFilter extends Component {
   render(){
     return (
       <div>
-        <button class="filterBtn" name="byDueDate" onClick={this.handleClick}>DUE DATE</button>
-        <button class="filterBtn" name="byPriorityLevel" onClick={this.handleClick}>PRIORITY LEVEL</button>
-        <button class="filterBtn" name="byNoDate" onClick={this.handleClick}>NO DATE</button>
-        <button class="filterBtn" name="by7DaysAndWaiting" onClick={this.handleClick}>7 DAYS & WAITING</button>
+        <button className="filterBtn" name="byDueDate" onClick={this.handleClick}>DUE DATE</button>
+        <button className="filterBtn" name="byPriorityLevel" onClick={this.handleClick}>PRIORITY LEVEL</button>
+        <button className="filterBtn" name="byNoDate" onClick={this.handleClick}>NO DATE</button>
+        <button className="filterBtn" name="by7DaysAndWaiting" onClick={this.handleClick}>7 DAYS & WAITING</button>
       </div>
     )
   }
