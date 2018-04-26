1a45a0dcfca3b5ab8da0739d97bc51c5b981df37
Jarek Figura <jarek.figura@icloud.com>
2018-04-26 16:59:09 +0200

    AddTaskPopup.js => skeleton added

--- src/components/AddTaskPopup/AddTaskPopup.js
+++ src/components/AddTaskPopup/AddTaskPopup.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import './AddTaskPopup.css'
 import TaskForm from "../TaskForm/TaskForm";
+import TaskPriority from "../TaskPriority/TaskPriority";
 import TaskAddButton from "../TaskAddButton/TaskAddButton";
+import TaskDueDay from "../TaskDueDay/TaskDueDay";
 
 class AddTaskPopup extends Component {
+  state = {
+    taskTitleText: '',
+    taskDescription: '',
+    tasks: [ ]
+  };
+
   render() {
     return (
       <div>
-        <TaskForm/>
+        <TaskForm />
+        <TaskPriority />
+        <TaskDueDay/>
         <TaskAddButton/>
       </div>
     )
