cacce880400c9a5d3cfdf9e1ed9631fdd966a8b1
Jarek Figura <jarek.figura@icloud.com>
2018-05-08 23:05:21 +0200

    TaskList.js => refactoring and cleaning

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -3,28 +3,18 @@ import './TaskList.css'
 import TaskContent from "../TaskContent/TaskContent";
 import TaskSearch from "../TaskSearch/TaskSearch";
 
-// import TaskEditForm from './TaskEditForm';
-
 class TaskList extends Component {
   state = {
-    editTaskId: null,
     searchPhrase: ''
   };
 
-  enterEditMode = taskId => {
-    this.setState({editTaskId: taskId})
-  };
-  exitEditMode = () => {
-    this.setState({editTaskId: null})
-  };
-
-  updateSearchPhrase = searchPhrase => this.setState({searchPhrase})
+  updateSearchPhrase = searchPhrase => this.setState({searchPhrase});
 
   render() {
 
     const tasks = this.props.tasks.filter(
       task => task.name.toLowerCase().includes(this.state.searchPhrase.toLowerCase())
-    )
+    );
     return (
       <div>
         <TaskSearch
@@ -32,27 +22,24 @@ class TaskList extends Component {
         />
         {
           tasks.length === 0 ?
-            <p>Brak wyników</p> :
-            <ul>
-              {
-                tasks.map(
-                  task => (
-                    <li key={task.id}>
-                        <TaskContent
-                        task={task}
-                        enterEditMode={this.enterEditMode}
-                        removeTask={this.props.removeTask}
-                        toggleTaskDone={this.props.toggleTaskDone}
-                        toggleTaskImportant={this.props.toggleTaskImportant}
-                        toggleShowEditTaskPopup={this.props.toggleShowEditTaskPopup}
-                        />
-                    </li>
-                  )
-                )
-              }
-            </ul>
+          <p>Brak wyników</p> :
+          <ul>
+          {
+            tasks.map(
+              task => (
+                <li key={task.id}>
+                  <TaskContent
+                    task={task}
+                    removeTask={this.props.removeTask}
+                    toggleTaskDone={this.props.toggleTaskDone}
+                    toggleShowEditTaskPopup={this.props.toggleShowEditTaskPopup}
+                  />
+                </li>
+              )
+            )
+          }
+          </ul>
         }
-
       </div>
     )
   }
