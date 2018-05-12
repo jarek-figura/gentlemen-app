fab5fb585f7751e2e369690747e0988e414022fc
Jarek Figura <jarek.figura@icloud.com>
2018-05-12 04:09:42 +0200

    Contenxt API => wyświetla się lista, ale trzeba naprawić dodawanie i edycję

---
+++ src/components/TaskApp/TaskApp.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import {withTasks} from "../contexts/Tasks";
+import TaskList from "../../components/TaskList/TaskList";
+import TaskFilter from "../../components/TaskFilter/TaskFilter";
+
+class TaskApp extends Component {
+  state = {
+    showOnlyNotDoneEnabled: false,
+    showOnlyDoneEnabled: false
+  };
+
+  render () {
+    return (
+      <div className="App">
+
+        {this.props.currentForm === null
+          ? <div>
+
+            <TaskList
+              tasks={this.props.tasks.filter(
+                task => this.props.showOnlyNotDoneEnabled === false
+                  ? true
+                  : task.isDone === false
+              ).filter(
+                task => this.props.showOnlyDoneEnabled === false
+                  ? true
+                  : task.isDone === true
+              )}
+            />
+
+            <nav className='nav-bottom'>
+              {/* add task */}
+              <button onClick={this.props.toggleShowAddTaskPopup}><strong>Dodaj<br/>zadanie</strong></button>
+
+              {/* filters */}
+              {this.state.showOnlyDoneEnabled === false
+                ? <button onClick={() => this.setState({
+                  showOnlyDoneEnabled: true,
+                  showOnlyNotDoneEnabled: false
+                })}>Pokaż<br/>zrobione</button>
+                : <button onClick={() => this.setState({
+                  showOnlyNotDoneEnabled: true,
+                  showOnlyDoneEnabled: false
+                })}>Pokaż<br/>niezrobione</button>
+              }
+
+              <button onClick={() => this.setState({
+                showOnlyNotDoneEnabled: false,
+                showOnlyDoneEnabled: false
+              })}>Pokaż<br/>wszystkie
+              </button>
+
+              <TaskFilter/>
+            </nav>
+          </div>
+          : this.props.displayForm(this.props.currentForm)
+        }
+      </div>
+    )
+  }
+}
+
+export default withTasks(TaskApp);
