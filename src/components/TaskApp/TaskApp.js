781d3842d25c5c4ddb2389dd4206faa2f1d42294
Jarek Figura <jarek.figura@icloud.com>
2018-05-13 20:47:55 +0200

    refactoring TaskApp -> TaskNav added

--- src/components/TaskApp/TaskApp.js
+++ src/components/TaskApp/TaskApp.js
@@ -1,44 +1,18 @@
 import React, {Component} from 'react';
 import './TaskApp.css'
 import {withTasks} from "../contexts/Tasks";
-import TaskList from "../../components/TaskList/TaskList";
-import TaskFilter from "../../components/TaskFilter/TaskFilter";
+import TaskList from "../TaskList/TaskList";
+import TaskNav from '../TaskNav/TaskNav';
 
 class TaskApp extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="task-app">
         {
           this.props.currentForm === null
             ? <div>
                 <TaskList/>
-
-                <nav className='nav-bottom'>
-                  {/* add task */}
-                  <button onClick={this.props.toggleShowAddTaskPopup}><strong>Dodaj<br/>zadanie</strong></button>
-
-                  {/* show done / not done */}
-                  {this.props.showOnlyDoneEnabled === false
-                    ? <button onClick={() => {
-                      this.props.showOnlyDone(true);
-                      this.props.showOnlyNotDone(false);
-                    }}>Pokaż<br/>zrobione</button>
-                    : <button onClick={() => {
-                      this.props.showOnlyDone(false);
-                      this.props.showOnlyNotDone(true);
-                    }}>Pokaż<br/>niezrobione</button>
-                  }
-
-                  {/* show all */}
-                  <button onClick={() => {
-                    this.props.showOnlyDone(false);
-                    this.props.showOnlyNotDone(false);
-                  }}>Pokaż<br/>wszystkie
-                  </button>
-
-                  {/* sort by dueDate / sort by priority */}
-                  <TaskFilter/>
-                </nav>
+                <TaskNav/>
             </div>
             : this.props.displayForm(this.props.currentForm)
         }
