d21b07f0637ab4558e85e1d1d0d23705ad71c734
Jarek Figura <jarek.figura@icloud.com>
2018-05-15 00:17:26 +0200

    TaskNav => changing button display text

--- src/components/TaskNav/TaskNav.js
+++ src/components/TaskNav/TaskNav.js
@@ -7,26 +7,25 @@ class TaskNav extends Component {
     return (
       <nav className='nav-bottom'>
         {/* add task */}
-        <button onClick={this.props.toggleShowAddTaskPopup}><strong>Dodaj<br/>zadanie</strong></button>
+        <button onClick={this.props.toggleShowAddTaskPopup}><strong>{`Dodaj\nzadanie`}</strong></button>
 
         {/* show done / not done */}
         {this.props.showOnlyDoneEnabled === false
           ? <button onClick={() => {
             this.props.showOnlyDone(true);
             this.props.showOnlyNotDone(false);
-          }}>Pokaż<br/>zrobione</button>
+          }}>{`Pokaż\nzrobione`}</button>
           : <button onClick={() => {
             this.props.showOnlyDone(false);
             this.props.showOnlyNotDone(true);
-          }}>Pokaż<br/>niezrobione</button>
+          }}>{`Pokaż\nniezrobione`}</button>
         }
 
         {/* show all */}
         <button onClick={() => {
           this.props.showOnlyDone(false);
           this.props.showOnlyNotDone(false);
-        }}>Pokaż<br/>wszystkie
-        </button>
+        }}>{`Pokaż\nwszystkie`}</button>
 
         {/* sort by dueDate / sort by priority */}
         <TaskFilter/>
