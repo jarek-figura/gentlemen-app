93e719828f73747c1e6e21d02846aa5b5890a39c
BaldiniPL <baldakamil@gmail.com>
2018-05-15 16:56:01 +0200

    start from here

--- src/components/TaskProgressBar/TaskProgeressBar.js
+++ src/components/TaskProgressBar/TaskProgeressBar.js
@@ -1,18 +1,16 @@
 import React from 'react'
+import './TaskProgressBar.css'
 
 class TaskProgeressBar extends React.Component {
   render () {
     return(
-      <div>
 
-        <div class={"ldBar"}
-             style={"width:100%;height:60px"}
+        <div className="ldBar"
+
         data-stroke={"data:ldbar/res,gradient(0,1,#9df,#9fd,#df9,#fd9)"}
         data-path={"M10 20Q20 15 30 20Q40 25 50 20Q60 15 70 20Q80 25 90 20"}
       ></div>
 
-
-      </div>
     )
   }
 }
