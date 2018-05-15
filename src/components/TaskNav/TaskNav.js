b340b2ab77b6e1af7cf5684996f893bb54fe5a00
Jarek Figura <jarek.figura@icloud.com>
2018-05-15 16:31:48 +0200

    Filtering by task isDone fixed

--- src/components/TaskNav/TaskNav.js
+++ src/components/TaskNav/TaskNav.js
@@ -1,42 +1,56 @@
 import React, {Component} from 'react';
+import './TaskNav.css'
 import {withTasks} from "../contexts/Tasks";
 import TaskFilter from '../TaskFilter/TaskFilter';
 
 class TaskNav extends Component {
+  state = {
+    buttonClicked: 'PW'
+  };
+
+  handleClick = (event) => {
+    this.setState({buttonClicked: event.currentTarget.name})
+  };
 
   render () {
-
     return (
       <nav className='nav-bottom'>
-
         <button
+          className="add"
           title="Dodaj zadanie"
           onClick={this.props.toggleShowAddTaskPopup}
-        ><strong>{`\u271a`}</strong></button>
+        ><strong>{`Dodaj zadanie`}</strong></button>
 
         <button
-          value='1'
+          name='PZ'
           title="Pokaż zrobione"
-          onClick={() => {
+          className={this.state.buttonClicked === 'PZ' ? 'clicked' : ''}
+          onClick={(event) => {
             this.props.showOnlyDone(true);
             this.props.showOnlyNotDone(false);
+            this.handleClick(event);
           }}
+          // onClick={this.handleClick}
         >{`PZ`}</button>
 
         <button
-          value='1'
+          name='PN'
           title="Pokaż niezrobione"
-          onClick={() => {
+          className={this.state.buttonClicked === 'PN' ? 'clicked' : ''}
+          onClick={(event) => {
           this.props.showOnlyDone(false);
           this.props.showOnlyNotDone(true);
+          this.handleClick(event);
         }}>{`PN`}</button>
 
         <button
-          value='3'
+          name='PW'
           title="Pokaż wszystkie"
-          onClick={() => {
+          className={this.state.buttonClicked === 'PW' ? 'clicked' : ''}
+          onClick={(event) => {
           this.props.showOnlyDone(false);
           this.props.showOnlyNotDone(false);
+          this.handleClick(event);
         }}>{`PW`}</button>
 
         {/* sort by dueDate / sort by priority */}
