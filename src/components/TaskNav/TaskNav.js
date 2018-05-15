139f96d822424ca990f5a7ce6a587785c083c54a
Jarek Figura <jarek.figura@icloud.com>
2018-05-15 03:08:07 +0200

    TaskNav => WIP: changing button display as radio buttons

--- src/components/TaskNav/TaskNav.js
+++ src/components/TaskNav/TaskNav.js
@@ -1,31 +1,43 @@
 import React, {Component} from 'react';
 import {withTasks} from "../contexts/Tasks";
-import TaskFilter from '../TaskFilter/TaskFilter'
+import TaskFilter from '../TaskFilter/TaskFilter';
 
 class TaskNav extends Component {
+
   render () {
+
     return (
       <nav className='nav-bottom'>
-        {/* add task */}
-        <button onClick={this.props.toggleShowAddTaskPopup}><strong>{`Dodaj\nzadanie`}</strong></button>
 
-        {/* show done / not done */}
-        {this.props.showOnlyDoneEnabled === false
-          ? <button onClick={() => {
+        <button
+          title="Dodaj zadanie"
+          onClick={this.props.toggleShowAddTaskPopup}
+        ><strong>{`\u271a`}</strong></button>
+
+        <button
+          value='1'
+          title="Pokaż zrobione"
+          onClick={() => {
             this.props.showOnlyDone(true);
             this.props.showOnlyNotDone(false);
-          }}>{`Pokaż\nzrobione`}</button>
-          : <button onClick={() => {
-            this.props.showOnlyDone(false);
-            this.props.showOnlyNotDone(true);
-          }}>{`Pokaż\nniezrobione`}</button>
-        }
+          }}
+        >{`PZ`}</button>
 
-        {/* show all */}
-        <button onClick={() => {
+        <button
+          value='1'
+          title="Pokaż niezrobione"
+          onClick={() => {
+          this.props.showOnlyDone(false);
+          this.props.showOnlyNotDone(true);
+        }}>{`PN`}</button>
+
+        <button
+          value='3'
+          title="Pokaż wszystkie"
+          onClick={() => {
           this.props.showOnlyDone(false);
           this.props.showOnlyNotDone(false);
-        }}>{`Pokaż\nwszystkie`}</button>
+        }}>{`PW`}</button>
 
         {/* sort by dueDate / sort by priority */}
         <TaskFilter/>
