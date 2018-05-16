f54e0578761f17b16696b7099ef99c27c8cc2af7
BaldiniPL <baldakamil@gmail.com>
2018-05-16 15:03:33 +0200

    progress bar deleted

--- src/components/TaskProgressBar/TaskProgeressBar.js
+++ src/components/TaskProgressBar/TaskProgeressBar.js
@@ -5,11 +5,7 @@ class TaskProgeressBar extends React.Component {
   render () {
     return(
 
-        <div className="ldBar"
 
-        data-stroke={"data:ldbar/res,gradient(0,1,#9df,#9fd,#df9,#fd9)"}
-        data-path={"M10 20Q20 15 30 20Q40 25 50 20Q60 15 70 20Q80 25 90 20"}
-      ></div>
 
     )
   }
