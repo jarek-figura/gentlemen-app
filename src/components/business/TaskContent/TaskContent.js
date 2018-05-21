d01702788dce123df78b354411462b1b06a3f270
Kuba <jkb.kam@gmail.com>
2018-05-22 00:25:39 +0200

    taks description works- have to change button

--- src/components/business/TaskContent/TaskContent.js
+++ src/components/business/TaskContent/TaskContent.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import moment from 'moment';
 import './TaskContent.css';
 import {withTasks} from "../../contexts/Tasks";
@@ -6,59 +6,78 @@ import TaskPriorityBar from "../TaskProgressBar/TaskPriorityBar";
 
 
 class TaskContent extends Component {
-  render() {
-    const task = this.props.task;
-    const taskDueDate = new Date(task.dueDate);
-    const today = new Date(Date.now());
-    const timeDiff = taskDueDate.getTime() - today.getTime();
-    const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-    const whatToRender = () =>{
-        if(diffDays <= -1 ){
-              return <p className="wrn">Po terminie</p>
-          } else if ( diffDays === 0) {
-              return <p className="wrnToday">Na dzisiaj</p>
-          } else {
-              return <TaskPriorityBar dueDate={task.dueDate} />
-          }
-      }
+state = {
+    showDesc: false
+}
+    handleClick = () => {
+        this.setState({
+            showDesc: !this.state.showDesc
+        });
+    };
 
-    return (
-      <div className={task.priority + ' ' + task.isDone + ' task'}>
+    render() {
+        const task = this.props.task;
+        const taskDueDate = new Date(task.dueDate);
+        const today = new Date(Date.now());
+        const timeDiff = taskDueDate.getTime() - today.getTime();
+        const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        const whatToRender = () => {
+            if (diffDays <= -1) {
+                return <p className="wrn">Po terminie</p>
+            } else if (diffDays === 0) {
+                return <p className="wrnToday">Na dzisiaj</p>
+            } else {
+                return <TaskPriorityBar dueDate={task.dueDate}/>
+            }
+        }
 
-        <div className="titles">
-          <strong>{task.name}</strong><br/>
-          {moment(task.dueDate).format('DD-MM-YYYY')}
+        return (
+            <div className={task.priority + ' ' + task.isDone + ' task'}>
 
-            {whatToRender()}
-
-        </div>
-
-        <div className="buttons">
-          <input className='done-checkbox'
-            title="Oznacz jako zrobione"
-            type="checkbox"
-            checked={task.isDone}
-            onChange={() => this.props.toggleTaskDone(task.id)}
-
-          />
-
-          <span>&nbsp;</span>
-          <button className='edit-button'
-            onClick={() => this.props.toggleShowEditTaskPopup(task.id)}
-          >edytuj</button>
-
-          <span>&nbsp;</span>
-          <button className='delete-button'
-            title="Usuń zadanie"
-            onClick={() => this.props.removeTask(task.id)}
-          >&times;</button>
+                <div className="titles">
+                    <strong>{task.name}</strong>
+                    <button
+                        className={"show-desc"}
+                        title={"show"}
+                        onClick={this.handleClick}
+                    >Show</button><br/>
+                    {moment(task.dueDate).format('DD-MM-YYYY')}<br/>
+                    {this.state.showDesc ?
+                        task.description
+                        : ''}
 
 
-        </div>
+                    {whatToRender()}
 
-      </div>
-    )
-  }
+                </div>
+
+                <div className="buttons">
+                    <input className='done-checkbox'
+                           title="Oznacz jako zrobione"
+                           type="checkbox"
+                           checked={task.isDone}
+                           onChange={() => this.props.toggleTaskDone(task.id)}
+
+                    />
+
+                    <span>&nbsp;</span>
+                    <button className='edit-button'
+                            onClick={() => this.props.toggleShowEditTaskPopup(task.id)}
+                    >edytuj
+                    </button>
+
+                    <span>&nbsp;</span>
+                    <button className='delete-button'
+                            title="Usuń zadanie"
+                            onClick={() => this.props.removeTask(task.id)}
+                    >&times;</button>
+
+
+                </div>
+
+            </div>
+        )
+    }
 }
 
 export default withTasks(TaskContent);
